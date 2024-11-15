d0203689342d649a6a1695345f7ec8ade2d7f58d
FuadHmdn <fuadhamidan.drive@gmail.com>
2024-11-15 17:04:35 +0700

    add profile user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Penyewaan;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class HomeController extends Controller
 {
@@ -17,6 +19,32 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function profile()
+    {
+        $user = auth()->user();
+
+        return view('user-profile', compact('user'));
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $user = Auth::user();
+
+        // Validasi input
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:users,email,' . $user->id,
+        ]);
+
+        // Update nama dan email
+        $user->name = $validated['name'];
+        $user->email = $validated['email'];
+        $user->save();
+
+        return redirect()->route('profile')->with('success', 'Profil berhasil diperbarui!');
+    }
+
 
     public function adminHome()
     {
